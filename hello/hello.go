946f089ba5c4a8045a2e72a7b6c97a720f0a1d82
JohannSuarez <johann.suarez92@gmail.com>
2022-04-05 01:40:37 -0700

    Finished tutorial

--- hello/hello.go
+++ hello/hello.go
@@ -14,8 +14,12 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    // A slice of names
+    names :=  []string{"Gladys", "Samantha", "Darrin"}
+
+
     // Request a greeting message
-    message, err := greetings.Hello("Lise")
+    message, err := greetings.Hellos(names)
 
     // If an error was returned, print it to the console and
     // exit the program.
