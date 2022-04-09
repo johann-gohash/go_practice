10ef148382e0648a2e0be20be02222b69724fc1b
Johann Suarez <johann.suarez92@gmail.com>
2022-04-08 19:24:04 -0700

    Go book

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,43 +1,43 @@
 package greetings
 
 import (
-    "errors"
-    "fmt"
-    "math/rand"
-    "time"
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 // init sets initial values for variables used in the function
 func init() {
-    rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 // randomFormat returns one of a set of greeting messages. The
 // returned message is selected at random
 
 func randomFormat() string {
-    // A slice of message formats.
-    formats := []string{
-        "Hi, %v. Welcome!",
-        "Great to see you, %v",
-        "Hey there %v!",
-    }
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hey there %v!",
+	}
 
-    // Return a randomly selected message format by specifying
-    // a random index for the slice of formats
-    return formats[rand.Intn(len(formats))]
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats
+	return formats[rand.Intn(len(formats))]
 }
 
 // Hello returns a greeting for the named person
 func Hello(name string) (string, error) {
-    // If no name was given, return an error with aa message.
-    if name == "" {
-        return "", errors.New("empty name")
-    }
+	// If no name was given, return an error with aa message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf(randomFormat(), name)
-    return message, nil
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
 }
 
 // Hellos returns a map that associates each of the named people
@@ -45,20 +45,20 @@ func Hello(name string) (string, error) {
 
 func Hellos(names []string) (map[string]string, error) {
 
-    // A map to associate names with messages.
-    // We have to initialize maps, we doing this by using make
-    messages := make(map[string]string)
-    // Loop through the received slice of names, calling
-    // the Hello function to get a message for each name.
+	// A map to associate names with messages.
+	// We have to initialize maps, we doing this by using make
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
 
-    for _, name := range names {
-        message, err := Hello(name)
-        if err != nil {
-            return nil, err
-        }
-        // In the map, associate the retrieved message with
-        // the name.
-        messages[name] = message
-    }
-    return messages, nil
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with
+		// the name.
+		messages[name] = message
+	}
+	return messages, nil
 }
---
+++ pointers/tutorial.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    x := 7
+    y := &x
+
+    *y += 2
+    fmt.Println(x)
+
+
+}
