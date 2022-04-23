4d7cd3160155f2961ac0a11a362db076798fee4c
Johann Suarez <johann.suarez92@gmail.com>
2022-04-22 20:52:06 -0700

    Updating practice files

--- file/read_file.go
+++ file/read_file.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 
-    content, err := ioutil.ReadFile("file.txt")
+    content, err := ioutil.ReadFile("filedir/file_0.txt")
 
     if err != nil {
         log.Fatal(err)
--- strings/string_prac.go
+++ strings/string_prac.go
@@ -13,14 +13,10 @@ type User struct {
 
 func main() {
 
-
     the_string := "Dima:Pass123"
-
     y := strings.Split(*&the_string, ":")
     // fmt.Println(y)
     // fmt.Print(reflect.TypeOf(y))
     z := User{y[0], y[1]}
     fmt.Println(z)
-
-
 }
